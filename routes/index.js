488d7b71dd0ecb2f28f1887cda16a46c4c477af0
jadeoakes1 <oakesjade@gmail.com>
2022-07-20 19:09:54 -0400

    index.js back to orig

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const port = 8080;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Word Counter' });
-});
-
-application.listen(port, () => {
-  console.log(`App listening on port ${port}`);
 });
 
 module.exports = router;
