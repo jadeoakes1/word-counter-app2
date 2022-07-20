219bd0b396056a353741cb4afedefc1a5e49abbb
jadeoakes1 <oakesjade@gmail.com>
2022-07-20 17:46:40 -0400

    changed route to 8080

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+const { application } = require('express');
 var express = require('express');
-var router = express.Router();
+var port = 8080;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Word Counter' });
 });
 
-module.exports = router;
+application.listen(port, () => {
+  console.log('App listening on port ' + {port});
+})
